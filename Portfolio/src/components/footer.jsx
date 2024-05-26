9c2ea689cf4c7f25b267bd787020a4906fa16b55
Orlando-pixel <peluza_1111@hotmail.com.ar>
2024-05-26 20:22:50 -0300

    Modificaciones de CSS

--- Portfolio/src/components/footer.jsx
+++ Portfolio/src/components/footer.jsx
@@ -4,10 +4,11 @@ import './footer.css';
 const Footer = () => {
   return (
     <footer>
-      <p>&copy; 2024 Orlando Agustin Menendez. Todos los derechos reservados.</p>
+      <p>&copy; 2024 Agustin Mz Company. Todos los derechos reservados.</p>
       <div className="social-links">
         <a href="https://www.facebook.com/agustin.menendez.9/" target="_blank" rel="noopener noreferrer">Facebook</a>
         <a href="https://www.linkedin.com/in/agust%C3%ADn-men%C3%A9ndez-8238061b1/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
       </div>
     </footer>
   );
--- Portfolio/src/components/main.jsx
+++ Portfolio/src/components/main.jsx
@@ -5,9 +5,11 @@ const Main = () => {
   return (
     <main>
       <div className="main-content">
-        <h2>Sobre Mí</h2>
+        <h2>Info general (Primer etapa)</h2>
         <p>Soy Orlando Agustin Menendez, estudiante de programación de la UTN-FRT. Estoy en constante aprendizaje y quiero poder superarme cada vez mas. Destacando en el area de Front end</p>
         <p>Me apasiona la programacion, asi tambien como el canto coral, el jockey, trekking entre otras actividades.</p>
+        <br></br>
+        <p>En esta pagina vas a encontrar todo una informacion general sobre mi persona, historia y curiosidades</p>
       </div>
     </main>
   );
